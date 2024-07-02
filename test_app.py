ed97728b645c584c9a1cc51f4b8676c301ee522f
MohamedHassan <mohamed.hassankamel.94@gmail.com>
2024-07-02 22:02:49 +0200

    update unit test

--- test_app.py
+++ test_app.py
@@ -1,27 +1,44 @@
-import json
 import unittest
-from unittest.mock import patch
 from app import app
 
-class TestFlaskApp(unittest.TestCase):
+class TestApp(unittest.TestCase):
+
     def setUp(self):
         self.app = app.test_client()
+        self.app.testing = True
 
     def test_index_route(self):
-        # Test that the index route returns status code 200
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Export', response.data)
+        self.assertIn(b'Import', response.data)
 
-    @patch('app.request')
-    def test_update_graph_route(self, mock_request):
-        # Test that the update_graph route returns status code 200 with mock request data
-        mock_request.form.get.return_value = 'selected_value'
-        response = self.app.post('/update_graph')
+    def test_update_year_graph_route(self):
+        response = self.app.post('/update_year_graph', data={'year': '2023', 'na_items[]': ['Export']})
         self.assertEqual(response.status_code, 200)
 
-        # Test that the response contains expected data
-        data = json.loads(response.data)
-        self.assertIn('graphJSON', data)
+    def test_update_graph_route(self):
+        response = self.app.post('/update_graph', data={'country': 'DE', 'na_items[]': ['Import']})
+        self.assertEqual(response.status_code, 200)
+
+    def test_country_route(self):
+        response = self.app.get('/country')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'DE', response.data)
+        self.assertIn(b'Export', response.data)
+        self.assertIn(b'Import', response.data)
+
+    def test_update_pie_charts_route(self):
+        response = self.app.post('/update_pie_charts', data={'year': '2023', 'country': 'DE'})
+        self.assertEqual(response.status_code, 200)
+        json_data = response.get_json()
+        self.assertIn('pieTracesJSON', json_data)
+
+    def test_update_combined_pie_charts_route(self):
+        response = self.app.post('/update_combined_pie_charts', json={'country': 'DE'})
+        self.assertEqual(response.status_code, 200)
+        json_data = response.get_json()
+        self.assertIn('combinedPieTracesJSON', json_data)
 
 if __name__ == '__main__':
     unittest.main()
